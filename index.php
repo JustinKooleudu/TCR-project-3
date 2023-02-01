ee26b9a818d91c66c0d13673c438b16434260c47
JustinKooledu <9019785@student.zadkine.nl>
2023-02-01 12:57:10 +0100

    1

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>gameINK</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="dark.css" id="dark">
+    <link rel="stylesheet" href="light.css" id="light">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
---
+++ java.js
@@ -0,0 +1,10 @@
+<script>
+function enableStylesheet (node) {
+  node.rel = 'stylesheet';
+}
+
+function disableStylesheet (node) {
+  node.rel = 'alternate stylesheet';
+}
+
+if 
