c38f803ff96702f85f0f44a3b650c3ce6add0002
JustinKooledu <9019785@student.zadkine.nl>
2023-02-22 15:01:47 +0100

    1

--- index.php
+++ index.php
@@ -3,124 +3,139 @@ include_once('head-footer/header.php');
 include_once('includes/functions.inc.php');
 include_once('includes/dbh.inc.php');
 ?>
-    <?php
-    if (isset($_POST['add'])){
-        if(!isset($_SESSION['userid'])) {
-            header("location: User/login.php");
-        }else{
-        if (isset($_SESSION['cart'])){
+<?php
+if (isset($_POST['add'])) {
+    if (!isset($_SESSION['userid'])) {
+        header("location: User/login.php");
+    } else {
+        if (isset($_SESSION['cart'])) {
             $_SESSION['CurrentGame'] = $_POST['productId'];
             header("location: User/game.php");
-        }else{
+        } else {
             $item_ary = array('productId' => $_POST['productId']);
             $_SESSION['cart'][0] = $item_ary;
         }
     }
 }
-    ?>
+?>
 <section id="oneIndex">
     <div class="main-banner" id="main-banner">
-    <div class="imgban" id="imgban4"></div>
-    <div class="imgban" id="imgban3"></div>
-    <div class="imgban" id="imgban2"></div>
-    <div class="imgban" id="imgban1"></div></div>
-        <div class="Side">
-            <?php 
-            if (isset($_SESSION['userid'])) {
-                echo '<h1 id="welcome">welcome '.$username.'</h1><div id="inform"><h1>you may want to play..</h1>';
-            }
-            else
-            {
-                echo '<h1 id="welcome">welcome</h1><div id="inform"><h1>you may want to play..</h1> ';
-            }
-            ?>
-            <div id="RecGames">
+        <div class="imgban" id="imgban4"></div>
+        <div class="imgban" id="imgban3"></div>
+        <div class="imgban" id="imgban2"></div>
+        <div class="imgban" id="imgban1"></div>
+    </div>
+    <div class="Side">
+        <?php
+        if (isset($_SESSION['userid'])) {
+            echo '<h1 id="welcome">welcome ' . $username . '</h1><div id="inform"><h1>you may want to play..</h1>';
+        } else {
+            echo '<h1 id="welcome">welcome</h1><div id="inform"><h1>you may want to play..</h1> ';
+        }
+        ?>
+        <div id="RecGames">
             <h1 id="recGameH1">Current game</h1>
             <p id="recGameP">current game info</p>
             <div class="banner-line-parent">
-            <div onclick="bn(1)" class="banner-line" id="bnrLine1"></div>
-            <div onclick="bn(2)" class="banner-line" id="bnrLine2"></div>
-            <div onclick="bn(3)" class="banner-line" id="bnrLine3"></div>
-            <div onclick="bn(4)" class="banner-line" id="bnrLine4"></div>
-            </div>
+                <div onclick="bn(1)" class="banner-line" id="bnrLine1"></div>
+                <div onclick="bn(2)" class="banner-line" id="bnrLine2"></div>
+                <div onclick="bn(3)" class="banner-line" id="bnrLine3"></div>
+                <div onclick="bn(4)" class="banner-line" id="bnrLine4"></div>
             </div>
         </div>
-    </section>
-    <div id="HeightCorrection"><div class="line"></div>
+    </div>
+</section>
+<div id="HeightCorrection">
+    <div class="line"></div>
     <div class="HeightCorrection-2">
-    <h1 id="txt-2">INK</h1><h1 id="txt-1">Game</h1>
-    <?php
-    if (isset($_SESSION['userid'])){
-    if ($uid === 17) {
-        echo '<h1 id="txt-1">Welcome Admin &nbsp;&nbsp;&nbsp;</h1><a id=admin-link href="admin.php">Admin TOOL</a>';
-    }
-    }
-    ?>
-    </div>
-    </div>
-
-    <!-- GAMES SECTION ----------------------------------------------------------------------------------------------------------------------->
-
-    <form action="" method="post"></form>
-    <section id="spotlight">
-        <nav id="Spotlight"><h1 id="spotlightTxt"><?php echo date('F') ?> Spotlight games</h1></nav>
-        <div class="spotlight1">
-            <?php
-            $result = getData($conn, "SELECT * FROM games WHERE State LIKE '%TRENDING%';");
-            $MaxCards = 0;
-            while ($MaxCards < 5) {
-                if($row = mysqli_fetch_assoc($result)){
-                    GameDisplay1($row['naam'],$row['prijs'],$row['image'],$row['Id']);
-                    $MaxCards++;
-                }
-            }
-            ?>
-        </div>
-        <div class="spotlight2">
+        <h1 id="txt-2">INK</h1>
+        <h1 id="txt-1">Game</h1>
         <?php
-            $result = getData($conn, "SELECT * FROM games WHERE State LIKE '%TRENDING2%';");
-            $MaxCards = 0;
-            while ($MaxCards < 3) {
-                if($row = mysqli_fetch_assoc($result)){
-                    GameDisplay2($row['naam'],$row['info'],$row['prijs'],$row['image'],$row['Id']);
-                    $MaxCards++;
-                }
+        if (isset($_SESSION['userid'])) {
+            if ($uid === 17) {
+                echo '<h1 id="txt-1">Welcome Admin &nbsp;&nbsp;&nbsp;</h1><a id=admin-link href="admin.php">Admin TOOL</a>';
             }
-            ?>
-        </div>
-        <nav id="Spotlight2"><h1 id="spotlightTxt">Populaire games</h1></nav>
-        <div class="spotlight2">
+        }
+        ?>
+    </div>
+</div>
+
+<!-- GAMES SECTION ----------------------------------------------------------------------------------------------------------------------->
+
+<form action="" method="post"></form>
+<section id="spotlight">
+    <nav id="Spotlight">
+        <h1 id="spotlightTxt">
+            <?php echo date('F') ?> Spotlight games
+        </h1>
+    </nav>
+    <div class="spotlight1">
         <?php
-            $result = getData($conn, "SELECT * FROM games WHERE State LIKE '%POPULAR%';");
-            $MaxCards = 0;
-            while ($MaxCards < 5) {
-                if($row = mysqli_fetch_assoc($result)){
-                    GameDisplay1($row['naam'],$row['prijs'],$row['image'],$row['Id']);
-                    $MaxCards++;
-                }
+        $result = getData($conn, "SELECT * FROM games WHERE State LIKE '%TRENDING%';");
+        $MaxCards = 0;
+        while ($MaxCards < 5) {
+            if ($row = mysqli_fetch_assoc($result)) {
+                GameDisplay1($row['naam'], $row['prijs'], $row['image'], $row['Id']);
+                $MaxCards++;
             }
-            ?>
-        </div>
-        <div class="Spotlight3"><nav id="Spotlight22"><h1 id="spotlightTxt2">Now Free games</h1></nav>
+        }
+        ?>
+    </div>
+    <div class="spotlight2">
+        <?php
+        $result = getData($conn, "SELECT * FROM games WHERE State LIKE '%TRENDING2%';");
+        $MaxCards = 0;
+        while ($MaxCards < 3) {
+            if ($row = mysqli_fetch_assoc($result)) {
+                GameDisplay2($row['naam'], $row['info'], $row['prijs'], $row['image'], $row['Id']);
+                $MaxCards++;
+            }
+        }
+        ?>
+    </div>
+    <nav id="Spotlight2">
+        <h1 id="spotlightTxt">Populaire games</h1>
+    </nav>
+    <div class="spotlight2">
+        <?php
+        $result = getData($conn, "SELECT * FROM games WHERE State LIKE '%POPULAR%';");
+        $MaxCards = 0;
+        while ($MaxCards < 5) {
+            if ($row = mysqli_fetch_assoc($result)) {
+                GameDisplay1($row['naam'], $row['prijs'], $row['image'], $row['Id']);
+                $MaxCards++;
+            }
+        }
+        ?>
+    </div>
+    <div class="Spotlight3">
+        <nav id="Spotlight22">
+            <h1 id="spotlightTxt2">Now Free games</h1>
+        </nav>
         <div class="spot4">
             <?php
             $result = getData($conn, "SELECT * FROM games WHERE State LIKE '%FREE%';");
             $MaxCards = 0;
             while ($MaxCards < 2) {
-                if($row = mysqli_fetch_assoc($result)){
-                    GameDisplay4($row['naam'],$row['info'],$row['prijs'],$row['prijs2'],$row['image'],$row['Id']);
+                if ($row = mysqli_fetch_assoc($result)) {
+                    GameDisplay4($row['naam'], $row['info'], $row['prijs'], $row['prijs2'], $row['image'], $row['Id']);
                     $MaxCards++;
                 }
             }
             ?>
-        </div></div>
+        </div>
+    </div>
 
-        <div class="Spotlight5"><img id="gameImage5" src="docs/GameIdCatalog.png">
-        <div class="exploreC"><h1 id="exploreCtitle">Explore our catalog</h1><h1 id="exploreCinfo">catalog info</h1><button id="exploreCbutton">BROWSE ALL</button></div></div>
-    </section>
+    <div class="Spotlight5"><img id="gameImage5" src="docs/GameIdCatalog.png">
+        <div class="exploreC">
+            <h1 id="exploreCtitle">Explore our catalog</h1>
+            <h1 id="exploreCinfo">catalog info</h1><button id="exploreCbutton">BROWSE ALL</button>
+        </div>
+    </div>
+</section>
 
-    <?php
-    echo '<script>
+<?php
+echo '<script>
     var bannerStatus = 1;
 
 function bn(nmr) {
