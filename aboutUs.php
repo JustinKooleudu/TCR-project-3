d56cc3129bf7beb1f4bf51339fcf8e458869e55d
JustinKooledu <9019785@student.zadkine.nl>
2023-03-27 13:23:40 +0200

    1

--- aboutUs.php
+++ aboutUs.php
@@ -8,27 +8,46 @@ include_once("head-footer/header.php");
 ?>
 
 <html>
-    <header></header>
-    <body>
-        <br><br><div class="us">
+<header></header>
+
+<body>
+    <br><br>
+    <div class="us">
         <h1>About us</h1>
         <p1>we are a startup located in silicon valley, we hope to give game to poeple and be a platform </p1><br>
-        <p1>where creators big and small can post their games for poeple to enjoy and have a great time and a great comunity.</p1>
-        <br><br><br></div>
-        <div class="team">
+        <p1>where creators big and small can post their games for poeple to enjoy and have a great time and a great
+            comunity.</p1>
+        <br><br><br>
+    </div>
+    <div class="team">
         <h2>Our team</h2>
-        <p2>The team is filled with kind people such as, john our team leader he makes sure that everything is going </p2><br>
-        <p2>as planned and helps people with problems. there is also burt he makes sure finaces are done correctly </p2><br>
-        <p2>there are a lot of people we could go thru like: amanda, sara, james, lilith, peter, chatGPT, kelith, </p2><br>
-        <p2>zanathar, felix, erik, sam, arthur, ect</p2></div>
-            <br><br><br><br><br><br><br><br>
-    </body>
+        <p2>The team is filled with kind people such as, john our team leader he makes sure that everything is going
+        </p2><br>
+        <p2>as planned and helps people with problems. there is also burt he makes sure finaces are done correctly </p2>
+        <br>
+        <p2>there are a lot of people we could go thru like: amanda, sara, james, lilith, peter, chatGPT, kelith, </p2>
+        <br>
+        <p2>zanathar, felix, erik, sam, arthur, ect</p2>
+    </div><br><br>
+    <div class="doel">
+        <h1>onze doelstellingen</h1>
+        <p1>we are proud to say that we are eco-vriendly and donaat 10% of our profit to good couses to make the world a
+            better place.</p1>
+    <div>
+    <br><br><br><br><br><br><br><br>
+</body>
+
 </html>
 <style>
     .us {
         margin-left: 25px;
     }
+
     .team {
+        margin-left: 25px;
+    }
+
+    .doel {
         margin-left: 25px;
     }
 </style>
